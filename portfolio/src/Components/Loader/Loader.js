5363bfa0e634e13cdfda2aa123f7ef3476c9bcc0
KrisSolanki <krissolanki28@gmail.com>
2024-06-07 23:26:38 +0530

    Loader added

--- portfolio/src/Components/Loader/Loader.js
+++ portfolio/src/Components/Loader/Loader.js
@@ -1,24 +1,51 @@
-// Loader.js
+// // Loader.js
+
+// import React from 'react';
+// import './Loader.css';
+// import { PacmanLoader } from 'react-spinners'
+
+// const Loader = () => {
+//   return (
+//     <div className="loader_container">
+//      <div className="K">
+
+//      K
+//     </div>
+//      <div className="circle">
+
+//      </div>
+//       {/* <PacmanLoader
+//   color="#0ba668"
+//   size={45}
+// /> */}
+ 
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
 
 import React from 'react';
 import './Loader.css';
-import { PacmanLoader } from 'react-spinners'
 
 const Loader = () => {
   return (
-    <div className="loader-container">
-      {/* <div className="circle-container">
-        <div className="circle">
-          <div className="inner-circle">K</div>
-        </div>
-      </div> */}
-      <PacmanLoader
-  color="#0ba668"
-  size={45}
-/>
- 
+    <div className="loader_container">
+      <div className="K">K</div>
+      <div className="circle">
+        <svg className='svg_loader' viewBox="0 0 200 200">
+          <circle
+            className="circle-progress"
+            cx="100"
+            cy="100"
+            r="94"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
 
 export default Loader;
+
