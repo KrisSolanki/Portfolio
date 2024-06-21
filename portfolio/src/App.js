3c46f7898ce8c941847f7caa4822336a49c35c04
KrisSolanki <krissolanki28@gmail.com>
2024-06-21 17:17:17 +0530

    simple commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Mainpage from './Page/Mainpage';
-
 function App() {
   return (
     <div className="App">
       <Mainpage/>
-      
     </div>
   );
 }
--- portfolio/src/Components/Content/Content.js
+++ portfolio/src/Components/Content/Content.js
@@ -2,8 +2,15 @@ import React from 'react'
 import './Content.css'
 import Rside from '../RSide/Rside'
 import Side from '../Side/Side'
+import { useTypewriter } from 'react-simple-typewriter';
 
 const Content = () => {
+    const {text}= useTypewriter({
+        words: ['coder', 'web developer'],
+        loop: true,  // Ensures looping
+        typeSpeed: 1000,
+        deleteSpeed: 400,
+    });
     return (
         <div className='container_content'>
             <div className="sub_content">
@@ -17,6 +24,10 @@ const Content = () => {
                     </div>
                     <div className="name">
                         <p>Kris Solanki</p>
+                    </div>
+                    <div className="typ">
+                        <p>I am a <span> web developer</span>
+                            </p>
                     </div>
                     <div className="introduction">
                         <p>I am a graduate with a Bachelor's degree in Computer Applications . With a keen interest in web development, I am dedicated to expanding my skills and knowledge in this field. I have experience with frameworks such as Reactjs and Django, which has furthered my passion for creating dynamic and responsive web applications.</p>
