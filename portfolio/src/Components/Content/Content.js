56b8854a5109613e92f088be8ddf17b013f2ac9b
KrisSolanki <krissolanki28@gmail.com>
2024-06-23 23:22:58 +0530

    simple commit

--- portfolio/src/Components/Content/Content.js
+++ portfolio/src/Components/Content/Content.js
@@ -2,15 +2,18 @@ import React from 'react'
 import './Content.css'
 import Rside from '../RSide/Rside'
 import Side from '../Side/Side'
-import { useTypewriter } from 'react-simple-typewriter';
+import { useTypewriter , Cursor} from 'react-simple-typewriter';
+import img from '../../Asset/ms.jpeg'
 
 const Content = () => {
-    const {text}= useTypewriter({
-        words: ['coder', 'web developer'],
+    const [text]= useTypewriter({
+        words: ['software engineer', 'web developer'],
         loop: true,  // Ensures looping
-        typeSpeed: 1000,
-        deleteSpeed: 400,
+        typeSpeed: 120,
+        deleteSpeed: 80,
     });
+    
+
     return (
         <div className='container_content'>
             <div className="sub_content">
@@ -26,15 +29,23 @@ const Content = () => {
                         <p>Kris Solanki</p>
                     </div>
                     <div className="typ">
-                        <p>I am a <span> web developer</span>
+                        <p>I am a<span> {text}</span>
+                        {/* <Cursor/> */}
                             </p>
                     </div>
                     <div className="introduction">
-                        <p>I am a graduate with a Bachelor's degree in Computer Applications . With a keen interest in web development, I am dedicated to expanding my skills and knowledge in this field. I have experience with frameworks such as Reactjs and Django, which has furthered my passion for creating dynamic and responsive web applications.</p>
+                        <p>
+                        I’m a web developer and software engineer based in India who specializes in building and sometimes designing web experiences. Graduated with a Bachelor's degree in Computer Applications. With a keen interest in web development, I am dedicated to expanding my skills and knowledge in this field. I focus on creating dynamic and responsive web applications, always exploring new technologies and tools to enhance my work.
+                        </p>
+                        </div>
                     </div>
 
+                        <div className="middle_2">
+                            <div className="img_container">
+                        <img src = {img} alt="hello"/> 
+                            </div>
                 </div>
-                </div>
+                </div> 
                 <Side/>
                 
             </div>
